fc5c34de44e36556fc50bb08a8c89dadeff8bdfc
Filippo Martini <zalumare@gmail.com>
2019-01-03 12:53:08 +0100

    Add condition to coount var to avoid possible divisions by 0.

--- mr/src/main/java/org/apache/mahout/cf/taste/impl/eval/OrderBasedRecommenderEvaluator.java
+++ mr/src/main/java/org/apache/mahout/cf/taste/impl/eval/OrderBasedRecommenderEvaluator.java
@@ -338,7 +338,7 @@ public final class OrderBasedRecommenderEvaluator {
           break;
         }
       }
-      if (vectorZ[i] != 0) {
+      if (vectorZ[i] != 0 && count != 0) {
         ranks[i] = (rank / count) * (vectorZ[i] < 0 ? -1 : 1);  // better be at least 1
         ranksAbs[i] = Math.abs(ranks[i]);
       }
